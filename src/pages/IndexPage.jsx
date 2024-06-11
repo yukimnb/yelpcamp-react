bf9ff852c69984cc6e2b0a048e62e0eb357390c7
yukimnb <yuki_manabe.dev@icloud.com>
2024-06-11 10:47:16 +0900

    ページ名を修正

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const TopPage = () => {
+export const IndexPage = () => {
   return (
     <>
       <StyledBody className="text-center text-white bg-dark">
--- src/routes/routesBasic.jsx
+++ src/routes/routesBasic.jsx
@@ -1,12 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 
-import { TopPage } from "../pages/TopPage";
+import { IndexPage } from "../pages/IndexPage";
 import { MainPage } from "../pages/MainPage";
 
 export const routesBasic = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<TopPage />} />
+      <Route path="/" element={<IndexPage />} />
       <Route path="/campgrounds" element={<MainPage />} />
     </>
   )
