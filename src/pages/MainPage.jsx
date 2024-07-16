c7995f2cfe5dcd4ab1fdaced5edeba35766cc353
yukimnb <yuki_manabe.dev@icloud.com>
2024-07-16 13:36:09 +0900

    SPAでのスクロールのリセットを追加

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import { Footer } from "../components/Footer";
 import { NavBar } from "../components/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../assets/css/starability.css";
 
@@ -10,6 +10,7 @@ export const MainPage = () => {
       <div className="d-flex flex-column vh-100">
         <NavBar />
         <main className="container mt-5">
+          <ScrollRestoration />
           <Outlet />
         </main>
         <Footer />
