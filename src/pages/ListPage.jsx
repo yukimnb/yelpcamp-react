f25074b8cfd03e623300b2e2fa6d968473f1199c
yukimnb <yuki_manabe.dev@icloud.com>
2024-08-11 01:22:57 +0900

    ListPageをMUI仕様に修正

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -4,10 +4,15 @@ import { getCampgroundsList } from "../apis/campground-api";
 import { Link } from "react-router-dom";
 import { ClusterMap } from "../components/ClusterMap";
 import { useErrorBoundary } from "react-error-boundary";
+import { Box, Typography, Button, useMediaQuery, useTheme, Card, CardMedia, CardContent } from "@mui/material";
+
+import SwapVertIcon from "@mui/icons-material/SwapVert";
 
 export const ListPage = () => {
   const [desc, setDesc] = useState(false);
   const { showBoundary } = useErrorBoundary();
+  const theme = useTheme();
+  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
 
   const { data, isSuccess } = useQuery("list", getCampgroundsList, {
     onSuccess: (data) => {
@@ -36,33 +41,55 @@ export const ListPage = () => {
   return (
     <>
       <ClusterMap newData={data} />
-      <div className="d-flex justify-content-between align-items-center mt-4">
-        <h1>キャンプ場一覧</h1>
-        <button onClick={handleSort} className="btn btn-danger">
-          ↑↓ {desc ? "古い順に並び替え" : "新しい順に並び替え"}
-        </button>
-      </div>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mt: 4, mb: 1 }}>
+        <Typography variant={isSm ? "h6" : "h5"} component="h1">
+          キャンプ場一覧
+        </Typography>
+        <Button variant="outlined" color="error" onClick={handleSort}>
+          <SwapVertIcon sx={{ mr: 1 }} />
+          <Typography sx={{ fontWeight: (theme) => theme.typography.fontWeightBold }}>
+            {isSm ? (desc ? "古い順" : "新しい順") : desc ? "古い順に並び替え" : "新しい順に並び替え"}
+          </Typography>
+        </Button>
+      </Box>
+
       {isSuccess &&
         data.map((object) => (
-          <div className="card mb-3" key={object.title}>
-            <div className="row">
-              <div className="col-lg-4">
-                <img src={object.image1} alt="" className="w-100 h-100" />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">{object.title}</h5>
-                  <p className="card-text">{object.description}</p>
-                  <p className="card-text">
-                    <small className="text-muted">{object.location}</small>
-                  </p>
-                  <Link to={`/campgrounds/${object.id}`} className="btn btn-primary">
-                    {object.title}の詳細
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Card
+            variant="outlined"
+            sx={{
+              display: "flex",
+              mb: 3,
+              height: { md: 200 },
+              flexDirection: { xs: "column", md: "row" },
+            }}
+            key={object.title}>
+            <CardMedia component="img" image={object.image1} alt={object.title} sx={{ width: { md: 300 } }} />
+            <CardContent
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                gap: { xs: 1, md: 0 },
+              }}>
+              <Typography variant="h5" component="h2">
+                {object.title}
+              </Typography>
+              <Typography variant="body1">
+                {object.description.length > 30 ? `${object.description.slice(0, 30)}...` : object.description}
+              </Typography>
+              <Typography variant="subtitle2" color="text.secondary">
+                {object.location}
+              </Typography>
+              <Button
+                variant="contained"
+                component={Link}
+                to={`/campgrounds/${object.id}`}
+                sx={{ alignSelf: { md: "flex-start" } }}>
+                {object.title}の詳細
+              </Button>
+            </CardContent>
+          </Card>
         ))}
     </>
   );
